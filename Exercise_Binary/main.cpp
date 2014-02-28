8169a4f6a550c72782d548a56ee46656ec52a914
sanyuancap <sanyuancap@foxmail.com>
2014-02-28 11:54:31 -0800

    Initial Commit

---
+++ Exercise_Binary/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Exercise_Binary
+//
+//  Created by ChengJY on 14-2-28.
+//  Copyright (c) 2014年 ChengJY. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
