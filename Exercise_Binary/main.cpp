f29cbc022827bfd6553ef81964a8f3628ea0e88b
sanyuancap <sanyuancap@foxmail.com>
2014-02-28 14:51:10 -0800

    a
    a

--- Exercise_Binary/main.cpp
+++ Exercise_Binary/main.cpp
@@ -8,11 +8,16 @@
 
 #include <iostream>
 
+
+//获得int型最大值
+int getMaxInt(){
+    return (1 << 31) - 1;//2147483647， 由于优先级关系，括号不可省略
+}
 int main(int argc, const char * argv[])
 {
 
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << getMaxInt()<<"\n";
     return 0;
 }
 
